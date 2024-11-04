8c35aa8a2b7dc0f7fdee54d8a59f17bf0d901b93
Mikko Lehtola <mikkolix@gmail.com>
2024-11-04 09:09:26 +0200

    muu´toksia apuaa

--- Assets/Julle/JullenSkriptit/SheepFarm.cs
+++ Assets/Julle/JullenSkriptit/SheepFarm.cs
@@ -3,14 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class SheepFarm : MonoBehaviour, IDamageable
 {
-    [SerializeField] private int hitpoints = 2;
+    public int hitpoints = 2;
+    int maxHitpoints; // Mikko
     public Sprite undestroyedSheepfarm;
     public Sprite destroyedSheepfarm;
     SpriteRenderer spriteRenderer;
     ScoreManager scoreManager;
 
+    public HealthBar healthBar; //Mikko
+
+
+    void Start() {
+        healthBar.UpdateBar(hitpoints/(float)maxHitpoints); //mikko
+    }
+
+
     void Awake()
     {
+        maxHitpoints = hitpoints; //Mikko
         spriteRenderer = GetComponent<SpriteRenderer>();
         scoreManager = FindAnyObjectByType<ScoreManager>();
     }
@@ -22,15 +32,20 @@ public class SheepFarm : MonoBehaviour, IDamageable
 
     }
 
+
     public void TakeDamage(int amount)
     {
+        
         if (hitpoints > 0)
         {
             hitpoints -= amount;
             scoreManager.AddScore(50);
+            healthBar.RevealTheBar(); // Mikko
+            healthBar.UpdateBar(hitpoints / (float)maxHitpoints);// Mikko
         }
         if (hitpoints <= 0)
         {
+            healthBar.HideTheBar(); // Mikko
             HandleDestruction();
         }
     }
